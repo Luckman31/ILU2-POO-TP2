648e84e26a59e8d128917d9479a93013acd11937
Luckman_31 <dasilva31120@gmail.com>
2024-11-11 14:38:17 +0100

    Fini

--- src/controleur/ControlAcheterProduit.java
+++ src/controleur/ControlAcheterProduit.java
@@ -28,5 +28,18 @@ public class ControlAcheterProduit {
 		}
 		return vendeur;
 	}
+	public String[] libererEtal(String nomVendeur) {
+		String[] donneesEtal = new String[5];
+		donneesEtal[0]=controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur).etatEtal()[0];;
+		donneesEtal[1]=controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur).etatEtal()[1];
+		donneesEtal[2]=controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur).etatEtal()[2];
+		donneesEtal[3]=controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur).etatEtal()[3];
+		donneesEtal[4]=controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur).etatEtal()[4];
+		return donneesEtal;
+	}
+	public int modifierQuantite(int quantite, String nomVendeur) {
+		Gaulois gaulois=village.trouverHabitant(nomVendeur);
+		return village.rechercherEtal(gaulois).acheterProduit(quantite);
+	}
 	
 }
--- src/frontiere/BoundaryAcheterProduit.java
+++ src/frontiere/BoundaryAcheterProduit.java
@@ -14,26 +14,38 @@ public class BoundaryAcheterProduit {
 
 	public void acheterProduit(String nomAcheteur) {
 		if(!controlAcheterProduit.verifierIdentite(nomAcheteur)) {
-			System.out.println("Je suis désolée "+nomAcheteur+" mais il faut être un habitant de notre village pour commercer ici");
+			System.out.println("Je suis dï¿½solï¿½e "+nomAcheteur+" mais il faut ï¿½tre un habitant de notre village pour commercer ici");
 			
 		}else {
 			StringBuilder acheter=new StringBuilder("Quel produit voulez-vous acheter ? ");
 			String produit=Clavier.entrerChaine(acheter.toString());
 			String[] vendeurs=controlAcheterProduit.trouverVendeur(produit);
 			if(vendeurs.length==0) {
-				System.out.println("Désolé, personne ne vend ce produit au marché");
+				System.out.println("Dï¿½solï¿½, personne ne vend ce produit au marchï¿½");
 			}else {
-				StringBuilder commercant=new StringBuilder("Chez quel commerçant voulez-vous acheter des fleurs ?");
+				StringBuilder commercant=new StringBuilder("Chez quel commerï¿½ant voulez-vous acheter des fleurs ?");
 				
 				int i;
 				for (i=0; i < vendeurs.length; i++) {
 					commercant.append("\n"+(i+1)+ " - " +vendeurs[i]);
 				}
 				int choixCommercant=Clavier.entrerEntier(commercant.toString())-1;
-				System.out.println(nomAcheteur+" se déplace jusqu'à l'étal du vendeur "+vendeurs[choixCommercant]);
-				StringBuilder quantite=new StringBuilder("Bonjour "+vendeurs[choixCommercant]);
+				System.out.println(nomAcheteur+" se dï¿½place jusqu'ï¿½ l'ï¿½tal du vendeur "+vendeurs[choixCommercant]);
+				StringBuilder quantite=new StringBuilder("Bonjour "+nomAcheteur);
 				quantite.append("\n Combien de "+produit+" voulez-vous acheter ?");
 				int choixQuantite=Clavier.entrerEntier(quantite.toString());
+				String[] etal=controlAcheterProduit.libererEtal(vendeurs[choixCommercant]);
+				int quantiteRestante=controlAcheterProduit.modifierQuantite(choixQuantite,vendeurs[choixCommercant]);
+				if(quantiteRestante==0) {
+					System.out.println(nomAcheteur+" veut acheter "+choixQuantite+" "+produit+", malheureusement il n'y en a plus !");
+				}
+				else if(quantiteRestante<choixQuantite) {
+					System.out.println(nomAcheteur+" veut acheter "+choixQuantite+" "+produit+", malheureusement "+ vendeurs[choixCommercant]+" n'en a plus que "+quantiteRestante+". "+nomAcheteur+" achÃ¨te tout le stock de "+vendeurs[choixCommercant]);
+					
+					;
+				}else {
+					System.out.println(nomAcheteur+" achÃ¨te "+choixQuantite+" "+produit+" Ã "+ vendeurs[choixCommercant]+".");
+				}
 				
 			}
 		}
